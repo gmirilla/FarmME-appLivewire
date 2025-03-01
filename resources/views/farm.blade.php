8d92ef113603976f18bd779c589102399d32152e
gmirilla <gmirilla@gmail.com>
2025-03-01 12:34:21 +0100

    Main Starting New Project

---
+++ resources/views/farm.blade.php
@@ -0,0 +1,40 @@
+<x-layouts.app>
+    {{-- Bootstrap assets --}}
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    
+<div>
+    {{-- Nothing in the world is as soft and yielding as water. --}}
+    To
+
+    <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Community</th>
+            <th scope="col">Farm Code</th>
+            <th scope="col">Farm Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+</div>
+</x-layouts.app>
