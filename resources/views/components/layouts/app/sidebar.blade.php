9518b750caaba646df96c11c4a5232c7e670086d
gmirilla <gmirilla@gmail.com>
2025-03-19 08:34:28 +0100

    added reports to the sidebar

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -15,6 +15,7 @@
                 <flux:navlist.group heading="Platform" class="grid">
                     <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>                 
                     <flux:navlist.item icon="photo" href='/farm'>Farms</flux:navlist.item>
+                    <flux:navlist.item icon="photo" href='/report'>Reports</flux:navlist.item>
 
                 </flux:navlist.group>
             </flux:navlist>
