60683ea8fea9cb8d48783821ef33eb2423de70d4
gmirilla <gmirilla@gmail.com>
2025-03-27 09:42:17 +0100

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,12 +3,50 @@
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+                <div class="card" style="background-color:green; color:white">
+                    <i class="fa-solid fa-user"></i> 
+                    <div class="card-body">
+                        <h3 class="card-title">150</h2>
+                      <p class="card-title" style="padding: 16px 0px 16px 0px">Number of Users</hp>
+                      <a href="#" style="background-color:rgba(0,0,0,.1); padding:0px 0px 0px 0px">
+                      <div  class="card-footer" style="text-align: center; ">
+                        <h5  style="color: white">More info</h5>
+                      </div>
+                      </a>
+                    </div>
+                  </div>
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+                
+                <div class="card" style="background-color:#17a2b8; color:white">
+                    <i class="fa-solid fa-user"></i> 
+                    <div class="card-body">
+                        <h3 class="card-title">50</h2>
+                      <p class="card-title" style="padding: 16px 0px 16px 0px">Number of Farms</hp>
+                        <a href="#" style="background-color:rgba(0,0,0,.1); padding:0px 0px 0px 0px">
+                      <div class="card-footer" style="text-align: center; opacity: 0.5;">
+                        <h5 style="color: white">More info</h5>
+                      </div>
+                      </a>
+                    </div>
+                  </div>
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
+                
+                <div class="card" style="background-color:#dc3545; color:white">
+                    <i class="fa-solid fa-user"></i> 
+                    <div class="card-body">
+                        <h3 class="card-title">30</h2>
+                      <p class="card-title" style="padding: 16px 0px 16px 0px">Number of Inspections Awaiting Approval</hp>
+                        <a href="#" style="background-color:rgba(0,0,0,.1); padding:0px 0px 0px 0px">
+                      <div class="card-footer" style="text-align: center; opacity: 0.5;">
+                        <h5 style="color: white">More info</h5>
+                      </div>
+                      </a>
+                    </div>
+                  </div>
             </div>
         </div>
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
