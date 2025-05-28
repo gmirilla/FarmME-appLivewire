9afac358cb1987d9aaee9c7cb67d1029263b48b6
gmirilla <gmirilla@gmail.com>
2025-05-28 18:11:23 +0100

    cosmtetic changes

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,5 +1,5 @@
-<div class="flex aspect-square size-8 items-center justify-center rounded-md bg-accent-content text-accent-foreground">
-    <x-app-logo-icon class="size-8  " />
+<div class="flex aspect-square size-10 items-center justify-center rounded-md  text-accent-foreground">
+    <x-app-logo-icon class="size-10" />
 </div>
 <div class="ml-1 grid flex-1 text-left text-sm">
     <span class="mb-0.5 truncate leading-none font-semibold" style="color:#F5F0E6">IMS</span>
