078511b0adda2486716dbb1dec3d5dfe548312f3
gmirilla <gmirilla@gmail.com>
2025-08-08 14:14:57 +0100

    fix for stashable issue

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -10,10 +10,6 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         @PwaHead
             <script src="https://cdn.jsdelivr.net/npm/dexie@3.2.2/dist/dexie.min.js"></script>
-    <script>
-  
-
-</script>
 <script src="/js/offline-handler.js"></script>
 <script src="/js/db.js"></script>
 
@@ -24,6 +20,8 @@
             color:#F5F0E6;
             border-color: #388E3C !important;
             text-decoration:none;
+           
+
 
         }
         .c-navitem-selected {
@@ -42,7 +40,7 @@
         $user = Auth::user();
     @endphp
     <body class="min-h-screen bg-white dark:bg-zinc-800" style="background: -webkit-gradient(linear, left top, right top, from(#f2eadc), to(#F5F0E6)) 0% 0% no-repeat padding-box">
-        <flux:sidebar sticky class="border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900" style="background-color:#388E3C ; color: #f5f0e6;" >
+        <flux:sidebar sticky stashable class="border-r border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900" style="background-color:#388E3C ; color: #f5f0e6" >
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
 
             <a href="{{ route('dashboard') }}" class="mr-5 flex items-center space-x-2" wire:navigate>
@@ -53,6 +51,7 @@
 
             <flux:navlist variant="outline" wire:ignore>
                 <flux:navlist.group heading="Platform" class="grid c-sidebar">
+
                      <flux:menu.separator />
                     @if (in_array($user->roles,['ADMINISTRATOR','INSPECTOR']) )
             
